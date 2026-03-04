26625ff73c137ee181cc0d34cd694719b80827ee
Hailey Gaylor <hailey.gaylor@acquia.com>
2026-03-03 16:12:17 -0800

    Initial commit with properfile

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '1e80b0559752ef08';
+  public const string VERSIONS_HASH = 'a315db647d58cf39';
 
 }
