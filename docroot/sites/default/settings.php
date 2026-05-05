a54026d636fbd17f948f12d0c8a3f2f41f044841
Hailey Gaylor <hailey.gaylor@acquia.com>
2026-05-04 21:58:39 -0700

    changed permissions

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -929,15 +929,18 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-if (file_exists('/var/www/site-php')) {
-  require '/var/www/site-php/eehaileygaylor/HGBlog-settings.inc';
-}
+// if (file_exists('/var/www/site-php')) {
+//   require '/var/www/site-php/eehaileygaylor/HGBlog-settings.inc';
+// }
 // $settings['trusted_host_patterns'] = [
 //   '^www\.HaileyGaylorBlog\.com$',
 //   '^.*\eehaileygaylordev.prod.acquia-sites\.com$',
 //   '^.*\eehaileygaylordev.prod.acquia-test\.com$',
 // ];
 
-if (file_exists('/var/www/site-php/')) {
-  require('/var/www/site-php/' . $_ENV['AH_SITE_GROUP'] . '/' . $_ENV['AH_SITE_GROUP'] . '-settings.inc');
+// if (file_exists('/var/www/site-php/')) {
+//   require('/var/www/site-php/' . $_ENV['AH_SITE_GROUP'] . '/' . $_ENV['AH_SITE_GROUP'] . '-settings.inc');
+// }
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/eehaileygaylordev/eehaileygaylordev-settings.inc');
 }
